952020b9fc801bbce8e2f2d2040536a8c04b6e17
Valerie <valeriyawoo@gmail.com>
2026-02-03 00:37:08 +0800

    feature-max-img-size

--- inference/preprocessing.py
+++ inference/preprocessing.py
@@ -1,21 +1,42 @@
+from typing import Tuple
+
 import torch
 import torch.nn.functional as F
 from PIL import Image
 from torchvision import transforms
 
 
+def pad_to_size_multiple_of_eight(x: torch.Tensor) -> torch.Tensor:
+    h, w = x.shape[2], x.shape[3]
+    pad_h = (8 - h % 8) % 8
+    pad_w = (8 - w % 8) % 8
+    x = F.pad(x, (0, pad_w, 0, pad_h), mode='reflect')
+    return x
+
+
+def get_coorect_size(img: Image.Image, max_size: int = 512) -> Tuple[int, int]:
+    w, h = img.size
+    new_h, new_w = h, w
+    if max(w, h) > max_size:
+        if w >= h:
+            new_w = max_size
+            new_h = int(h * max_size / w)
+        else:
+            new_h = max_size
+            new_w = int(w * max_size / h)
+
+    return new_h, new_w
+
+
 def preprocess_image(img: Image.Image, device: torch.device) -> torch.Tensor:
+    size = get_coorect_size(img)
     transform = transforms.Compose(
         [
+            transforms.Resize(size),
             transforms.ToTensor(),
             transforms.Lambda(lambda x: x * 2 - 1),  # [-1, 1]
         ]
     )
     x = transform(img.convert('RGB')).unsqueeze(0).to(device)
 
-    # padding to a size multiple of 8
-    h, w = x.shape[2], x.shape[3]
-    pad_h = (8 - h % 8) % 8
-    pad_w = (8 - w % 8) % 8
-    x = F.pad(x, (0, pad_w, 0, pad_h), mode='reflect')
-    return x
+    return pad_to_size_multiple_of_eight(x)
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,7 +1,32 @@
 import torch
 from PIL import Image
+from torchvision import transforms
 
-from style_transfer.inference.preprocessing import preprocess_image
+from style_transfer.inference.preprocessing import preprocess_image, pad_to_size_multiple_of_eight, get_coorect_size
+
+
+def test_get_coorect_size():
+    img = Image.new('RGB', (843, 954))
+    size = get_coorect_size(img)
+    assert size == (512, 452)
+
+
+def test_pad_to_size_multiple_of_eight():
+    img = Image.new('RGB', (257, 259))
+    device = torch.device('cpu')
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    x = transform(img.convert('RGB')).unsqueeze(0).to(device)
+
+    assert isinstance(x, torch.Tensor)
+
+    x = pad_to_size_multiple_of_eight(x)
+
+    assert isinstance(x, torch.Tensor)
+    assert x.shape[0] == 1
+    assert x.shape[1] == 3
+    assert x.shape[2] % 8 == 0
+    assert x.shape[3] % 8 == 0
 
 
 def test_preprocess_image_shape_and_range():
@@ -15,5 +40,21 @@ def test_preprocess_image_shape_and_range():
     assert x.shape[1] == 3
     assert x.shape[2] % 8 == 0
     assert x.shape[3] % 8 == 0
+    assert x.min() >= -1
+    assert x.max() <= 1
+
+
+def test_preprocess_image_too_big():
+    h, w = 1024, 512
+    img = Image.new('RGB', (w, h))
+    device = torch.device('cpu')
+
+    x = preprocess_image(img, device)
+
+    assert isinstance(x, torch.Tensor)
+    assert x.shape[0] == 1
+    assert x.shape[1] == 3
+    assert x.shape[2] == h / 2
+    assert x.shape[3] == w / 2
     assert x.min() >= -1
     assert x.max() <= 1
